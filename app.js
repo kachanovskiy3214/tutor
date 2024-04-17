23a7528e7d6666288142803f6b88b6b2dd1e1812
kachanovskiy3214 <kachanovskiy3214@gmail.com>
2024-04-17 10:26:37 +0300

    change_message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = 80;
-
+consy constant=100
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
